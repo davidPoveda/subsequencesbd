7fc4f28455f633059a0c84e6070327d9732010af
davidPoveda <davidmpv01@gmail.com>
2025-03-01 12:59:40 -0500

    Commit inicial

---
+++ subsequencesbd/src/main/java/com/dpoveda/servicio/subsequencesbd/subsequencesbd/SubsequencesbdApplication.java
@@ -0,0 +1,13 @@
+package com.dpoveda.servicio.subsequencesbd.subsequencesbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsequencesbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsequencesbdApplication.class, args);
+	}
+
+}
---
+++ subsequencesbd/src/test/java/com/dpoveda/servicio/subsequencesbd/subsequencesbd/SubsequencesbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpoveda.servicio.subsequencesbd.subsequencesbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsequencesbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
